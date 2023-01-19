0dc0ee77b4e5a572c232343ef35e1416f5e63a09
mk3058 <kmk3058@icloud.com>
2023-01-19 14:45:43 +0900

    command 처리 구현중

--- process/command.c
+++ process/command.c
@@ -6,7 +6,33 @@
 /*   By: minkyuki <minkyuki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 17:55:08 by minkyuki          #+#    #+#             */
-/*   Updated: 2023/01/18 17:56:54 by minkyuki         ###   ########.fr       */
+/*   Updated: 2023/01/19 12:42:19 by minkyuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/process.h"
+
+void	execute_cmd(t_cmd *cmd, char **envp, int child_num, int **fd)
+{
+	char	*path;
+	int		stdout_backup;
+
+	
+}
+
+static char	*find_path(char *cmd, char **envp)
+{
+	char	**env_path;
+	char	*file_path;
+	char	*cmd_path;
+	int		i;
+
+	if (access(cmd, R_OK | X_OK) == 0)
+		return (ft_strdup(cmd));
+	
+}
+
+static char	**parse_envp(char *envp)
+{
+	
+}
