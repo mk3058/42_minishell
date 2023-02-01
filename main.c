d50292aca305dc0778e900e3a4b9eaa1e86936c4
mk3058 <kmk3058@icloud.com>
2023-02-01 16:42:09 +0900

    불필요한 주석 제거

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: minkyuki <minkyuki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 20:26:06 by minkyu            #+#    #+#             */
-/*   Updated: 2023/02/01 16:19:56 by minkyuki         ###   ########.fr       */
+/*   Updated: 2023/02/01 16:41:52 by minkyuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,6 @@
 #include "includes/process.h"
 
 t_env *g_env;
-
-/** here_doc
- * 시그널 처리 -> 자식 프로세스에서 처리하는걸로 바꿔야함
- * 환경변수 치환 전에 전처리 필요
- * 	-> 따옴표 존재하는 경우 ", ' 상관없이 하나만 제거하고 나머지는 limiter처리 
-*/
-
-/** exit_stat
- * 시그널 종료시 exit_stat에 해당 시그널 넘버가 더해져야 함
- * 	-> exit_stat 변수를 env 전역변수에 추가
-*/
 
 int main(int argc, char **argv, char **envp)
 {
