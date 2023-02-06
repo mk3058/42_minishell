881116772ceae508cfb9941a40fd5314538d30c0
Bokyeong Jung <bojung@c3r5s7.42seoul.kr>
2023-02-06 17:17:33 +0900

    [bugfix] redirect 뒤 pipe가 오면 에러처리되는 문제 수정

--- parsing/parse_err.c
+++ parsing/parse_err.c
@@ -28,9 +28,11 @@ int	check_error(char **token)
 	while (*token)
 	{
 		if (is_cmd(*token, 0) == REDIR || is_cmd(*token, 0) == DOUBLED_REDIR)
-			if (*(token + 1) == NULL || is_cmd(*(token + 1), 0) == DOUBLED_REDIR
-				|| is_cmd(*(token + 1), 0) == REDIR || !is_cmd(*(token + 2), 0))
+		{
+			if (*(token + 1) == NULL || is_cmd(*(token + 1), 0) > 2
+				|| (is_cmd(*(token + 1), 0) != PIPE && !is_cmd(*(token + 2), 0)))
 				return (print_err("parse error near command"));
+		}
 		if (is_in_quote(*token, ft_strlen(*token)))
 			return (print_err("parse error near quote"));
 		if (err_special_char(*token) == -1)
